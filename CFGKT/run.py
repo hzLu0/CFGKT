c0eeed82b832428bb60d8444022bed1dc0417535
KTreserch <164134390+hzLu0@users.noreply.github.com>
2024-07-26 20:01:13 +0800

    Update run.py

--- CFGKT/run.py
+++ CFGKT/run.py
@@ -24,15 +24,13 @@ def run(args):
     sub_problems = {p: i for i, p in enumerate(raw_problem)}
     train_df['ucid'] = train_df['ucid'].map(sub_skills)
     train_df['upid'] = train_df['upid'].map(sub_problems)
-    skills = train_df.upid.unique()
-    n_skills = len(skills)
-    n_cats = len(train_df.ucid.unique())
-    print("no. of skills :", n_skills)
-    print("no. of categories: ", n_cats)
+    n_ques = len(train_df.upid.unique())
+    n_conc = len(train_df.ucid.unique())
+    print("no. of questions :", n_ques)
+    print("no. of concepts: ", n_conc)
     print("shape after exlusion:", train_df.shape)
 
     train_df['total_sec_taken'] = train_df['total_sec_taken'].clip(0, 300)
-
     train_df['timestamp_TW'] = train_df['timestamp_TW'].str[:-4]
     train_df['timestamp_TW'] = pd.to_datetime(train_df['timestamp_TW'])
     train_df['timestamp_TW'] = train_df['timestamp_TW'].astype(int) / (10 ** 9)
@@ -46,28 +44,22 @@ def run(args):
     train, val = train_test_split(group, test_size=0.2)
     val, test = train_test_split(val,test_size=0.5)
     print("train size: ", train.shape, "validation size: ", val.shape, "Test size: ",test.shape)
-    train_dataset = CFGKT_dataset(train.values, n_skills=n_skills, n_concept=n_cats, max_seq=args.max_len)
-    val_dataset = CFGKT_dataset(val.values, n_skills=n_skills, n_concept=n_cats, max_seq=args.max_len)
-    test_dataset = CFGKT_dataset(test.values, n_skills=n_skills, n_concept=n_cats, max_seq=args.max_len)
+    train_dataset = CFGKT_dataset(train.values, n_skills=n_ques, n_concept=n_conc, max_seq=args.max_len)
+    val_dataset = CFGKT_dataset(val.values, n_skills=n_ques, n_concept=n_conc, max_seq=args.max_len)
+    test_dataset = CFGKT_dataset(test.values, n_skills=n_ques, n_concept=n_conc, max_seq=args.max_len)
     train_loader = DataLoader(train_dataset,
                               batch_size=args.batch_size,
-                              num_workers=2,
-                              shuffle=True,
-                              worker_init_fn=np.random.seed(seed=300))
+                              shuffle=True)
     val_loader = DataLoader(val_dataset,
                             batch_size=args.batch_size,
-                            num_workers=2,
-                            shuffle=False,
-                            worker_init_fn=np.random.seed(seed=300))
+                            shuffle=False)
     test_loader = DataLoader(test_dataset,
                              batch_size=args.batch_size,
-                             num_workers=2,
-                             shuffle=False,
-                             worker_init_fn=np.random.seed(seed=300))
+                             shuffle=False)
 
     del train_dataset, val_dataset, test_dataset
     gc.collect()
-    KT_model = CFGKT(n_question=n_cats, n_pid=n_skills, d_model=args.embed_dim, n_blocks=args.n_blocks,
+    KT_model = CFGKT(n_concepts=n_conc, n_pid=n_ques, d_model=args.embed_dim, n_blocks=args.n_blocks,
                  kq_same = args.kq_same, dropout=args.dropout, model_type='CFGKT',memory_size=args.memory_size,
                final_fc_dim=args.final_fc_dim,n_heads=args.n_heads,d_ff=args.d_ff,
                time=args.time,interval=args.interval)
